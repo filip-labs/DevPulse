9649fb6ac1abfe3e609f7cf4403781f537c80005
GitHub Action <action@github.com>
2026-05-19 11:43:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/filiplabs/devpulse/MyBundle.kt
+++ src/main/kotlin/com/github/filiplabs/devpulse/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.filiplabs.devpulse
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/filiplabs/devpulse/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/filiplabs/devpulse/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.filiplabs.devpulse.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project

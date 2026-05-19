c296c5c2733fa8ac8953afaca7e282e6ca288e36
filip-cvetkovic <filip.cvetkovic@inmind.rs>
2026-05-19 14:24:32 +0200

    Added DevPulse tool window

---
+++ src/main/kotlin/com/github/filiplabs/devpulse/toolWindow/DevPulseToolWindowFactory.kt
@@ -0,0 +1,20 @@
+package com.github.filiplabs.devpulse.toolWindow
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBPanel
+import com.intellij.ui.content.ContentFactory
+
+class DevPulseToolWindowFactory : ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val panel = JBPanel<JBPanel<*>>()
+        panel.add(JBLabel("DevPulse"))
+        val content = ContentFactory.getInstance().createContent(panel, null, false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+    override fun shouldBeAvailable(project: Project) = true
+}
